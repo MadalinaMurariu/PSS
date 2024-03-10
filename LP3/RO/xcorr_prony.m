0388ce8bea99afb35d0419bb85a30f043aee206e
MadalinaMurariu <madalina-giorgiana.murariu@student.tuiasi.ro>
2024-03-10 16:38:19 +0200

    lab3 - functions

--- LP3/RO/xcorr_prony.m
+++ LP3/RO/xcorr_prony.m
@@ -1,11 +1,13 @@
 function r = xcorr_prony(x, k, l, M)
 
-x(1 : M+1-max(k,l)) = 0; %This line sets the first M+1-max(k,l) elements of the vector x to zero.
-rxx=xcorr(x); % Calculates the partial autocorrelation, the full vector x
-L=length(x); %This line calculates the length of the vector x and stores it in the variable L
+x(1 : M+1-max(k,l)) = 0; %ceastă linie setează primele M+1-max(k,l) elemente ale vectorului x la zero
+rxx=xcorr(x);
+L=length(x); %Această linie calculează lungimea vectorului x și o stochează în variabila L
 
 %rxx[0]=rxx[L] in Matlab
-%We will position ourselves relative to 0.
-r = rxx(L+k-l); 
+
+r = rxx(L+k-l);
+%Aici, funcția întoarce valoarea autocorelației parțiale la o anumită distanță, 
+% specificată de k și l. 
 
 end
